fa033ba3775ed2372da1ecb28ebb41dfe8b653ac
tomsihap <sihapanya.thomas@gmail.com>
2019-01-08 17:16:50 +0100

    Create fonctions-fonctions_recursives.php

---
+++ TD-01 - Structures/fonctions-fonctions_recursives.php
@@ -0,0 +1,254 @@
+<?php
+
+
+$quatreCube = 4*4*4;
+
+$cinqCube = 5*5*5;
+
+
+function cube($x) {
+    return $x*$x*$x;
+}
+
+
+$sixCube    = cube(6);
+$septCube   = cube(7);
+
+//$qteUsers = PDO('select count(*) from users');
+//$usersCube = cube($qteUsers);
+
+
+function afficheEnGras($t) {
+    return "<strong>" . $t . "</strong>";
+}
+
+echo afficheEnGras('thomas');
+
+
+function surface($longueur, $largeur) {
+
+    $s = $longueur * $largeur;
+
+    return $s;
+
+}
+
+echo "Votre appartement mesure " . surface(5, 3) . "m²";
+echo '<br>';
+
+
+function volumeCylindre($rayon, $hauteur) {
+
+    $rayonCarre = $rayon * $rayon;
+    $valeurDePi = pi();
+
+    $calculComplet = $valeurDePi * $rayonCarre * $hauteur;
+
+    return $calculComplet;
+
+}
+
+echo volumeCylindre(5, 3);
+
+echo "<br>";
+
+
+$disques = [
+    "highway to hell",
+    "ok computer",
+    "best of",
+    "anthologie",
+    "bonne année",
+    "live"
+];
+
+$legumes = [
+    "potirons",
+    "tomates",
+    "oignons",
+    "patates",
+    "poivrons"
+];
+
+function convertArrayToList($tableauQuelconque) {
+    
+    echo "<ul>";
+        foreach ($tableauQuelconque as $k => $v) {
+            echo "<li>" . $v . "</li>";
+        }
+    echo "</ul>";
+}
+
+
+convertArrayToList($disques);
+convertArrayToList($legumes);
+
+$legumes = [
+    'printemps'  => ['asperge', 'blette', 'navet', 'radis'],
+    'ete'        => ['comcombre', 'courgette', 'tomate', 'brocoli'],
+    'automne'    => ['fenouil', 'haricot', 'oignon', 'poireau'],
+    'hiver'      => ['potiron', 'topinampour', 'salsifi', 'betterave'],
+];
+
+$disquesTitres = [
+    "highway to hell"   => ['titre1', 'titre2', 'titre3'],
+    "ok computer"       => ['titre1', 'titre2', 'titre3'],
+    "best of"           => ['titre1', 'titre2', 'titre3'],
+    "anthologie"        => ['titre1', 'titre2', 'titre3'],
+    "bonne année"       => ['titre1', 'titre2', 'titre3'],
+    "live"              => ['titre1', 'titre2', 'titre3']
+];
+
+function convertArray2NiveauToList($tableauQuelconque) {
+
+    echo "<ul>";
+        foreach ($tableauQuelconque as $k => $v) {
+            echo "<li>" . $k . "</li>";
+
+            echo "<li>";
+                echo "<ul>";
+                    foreach ($v as $kk => $vv) {
+                        echo "<li>" . $vv . "</li>";
+                    }
+                echo "</ul>";
+            echo "</li>";
+        }
+    echo "</ul>";
+
+}
+
+
+convertArray2NiveauToList($legumes);
+convertArray2NiveauToList($disquesTitres);
+
+
+$disquesTitresCarac = [
+    "highway to hell"   => [
+        'titre1' => [
+            "duree" => '2:30',
+            "artistes" => "John Doe feat. Master T",
+        ],
+        'titre2' => [
+            "duree" => '2:30',
+            "artistes" => "John Doe feat. Master T",
+        ], 
+        'titre3' => [
+            "duree" => '2:30',
+            "artistes" => "John Doe feat. Master T",
+        ]
+    ],
+    "ok computer"       => [
+        'titre1' => [
+            "duree" => '2:30',
+            "artistes" => "John Doe feat. Master T",
+        ],
+        'titre2' => [
+            "duree" => '2:30',
+            "artistes" => "John Doe feat. Master T",
+        ], 
+        'titre3' => [
+            "duree" => '2:30',
+            "artistes" => "John Doe feat. Master T",
+        ]
+    ],
+    "best of"           => [
+        'titre1' => [
+            "duree" => '2:30',
+            "artistes" => "John Doe feat. Master T",
+        ],
+        'titre2' => [
+            "duree" => '2:30',
+            "artistes" => "John Doe feat. Master T",
+        ], 
+        'titre3' => [
+            "duree" => '2:30',
+            "artistes" => "John Doe feat. Master T",
+        ]
+    ],
+    "anthologie"        => [
+        'titre1' => [
+            "duree" => '2:30',
+            "artistes" => "John Doe feat. Master T",
+        ],
+        'titre2' => [
+            "duree" => '2:30',
+            "artistes" => "John Doe feat. Master T",
+        ], 
+        'titre3' => [
+            "duree" => '2:30',
+            "artistes" => "John Doe feat. Master T",
+        ]
+    ],
+    "bonne année"       => [
+        'titre1' => [
+            "duree" => '2:30',
+            "artistes" => "John Doe feat. Master T",
+        ],
+        'titre2' => [
+            "duree" => '2:30',
+            "artistes" => "John Doe feat. Master T",
+        ], 
+        'titre3' => [
+            "duree" => '2:30',
+            "artistes" => "John Doe feat. Master T",
+        ]
+    ],
+    "live"              => [
+        'titre1' => [
+            "duree" => '2:30',
+            "artistes" => "John Doe feat. Master T",
+        ],
+        'titre2' => [
+            "duree" => '2:30',
+            "artistes" => "John Doe feat. Master T",
+        ], 
+        'titre3' => [
+            "duree" => '2:30',
+            "artistes" => "John Doe feat. Master T",
+        ]
+        ]
+];
+
+
+/**
+ * On affiche l'aperçu de notre tableau récursif pour vérifier l'état des données
+ */
+echo "<pre>";
+print_r($disquesTitresCarac);
+echo "</pre>";
+
+
+function convertTableauxRecursifs($tableauQuelconque) {
+
+    // On ouvre le tableau
+    echo "<ul>";
+
+        // Pour chaque élément du tableau en tant que clé=>valeur (key=>potentielSousTableau)
+        foreach ($tableauQuelconque as $key => $potentielSousTableau) {
+
+            // On affiche déjà
+            echo "<li>" . $key . "</li>";
+
+            // si $potentielSousTableau est effectivement un sous tableau (is_array)...
+            if (is_array($potentielSousTableau)) {
+
+                // Alors il repasse dans la même fonction !
+                // C'est à dire qu'on réouvre un <ul> (voir ci-dessus)
+                // et qu'on reteste s'il a des enfants sous-tableaux.
+                echo "<li>";
+                convertTableauxRecursifs($potentielSousTableau);
+                echo "</li>";
+            }
+
+            // Sinon, si ce n'est pas un tableau...
+            else {
+
+                // Alors c'est une valeur (string/bool/int), on l'affiche.
+                echo "<li>" . $potentielSousTableau . "</li>";
+            }
+        }
+
+    echo "</ul>";
+}
+
+convertTableauxRecursifs($disquesTitresCarac);
