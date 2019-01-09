36b1fe9334eb6c0fd5bac02b6042b8d1cc16f02e
tomsihap <sihapanya.thomas@gmail.com>
2019-01-09 15:22:51 +0100

    Create login.php

---
+++ TD-03 - Projet - Login Page/login.php
@@ -0,0 +1,39 @@
+<?php
+
+echo "<pre>";
+var_dump($_POST);
+echo "</pre>";
+
+$emailValide = "admin@gmail.com";
+$passwordValide = "qsdfgh";
+
+
+if( isset($_POST['email']) && $_POST['email'] === $emailValide ) {
+    // Cas où l'email est correct
+
+    if ( isset( $_POST['password'] ) && $_POST['password'] === $passwordValide ) {
+        // Cas où l'utilisateur est loggué
+
+        ?>
+            <h3>Bienvenue sur votre profil, <?= $_POST['email']; ?></h3>
+            <p>Lorem ipsum dolor sit amet <strong>consectetur</strong> adipisicing elit. Autem praesentium fugiat dolores culpa id laborum atque neque, quis sunt voluptatum ullam vero quos voluptatem dignissimos repudiandae maiores inventore necessitatibus? Nemo!</p>
+
+        <?php
+    }
+
+    else {
+
+        ?>
+            <p><strong>Attention :</strong> le mot de passe est incorrect !</p>
+        <?php
+        // Cas où l'utilisateur existe mais le mot de passe n'est pas correct
+    }
+
+}
+else {
+    // Cas où l'e-mail n'existe pas
+    ?>
+            <p><strong>Attention :</strong> l'email n'existe pas !</p>
+
+    <?php
+}
