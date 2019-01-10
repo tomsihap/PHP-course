c49bcf162d2f6a9a81f4c7bbf32ffbd728e1bafa
tomsihap <sihapanya.thomas@gmail.com>
2019-01-10 15:35:37 +0100

    Update login.php

--- TD-03 - Projet - Login Page/login.php
+++ TD-03 - Projet - Login Page/login.php
@@ -1,22 +1,7 @@
 <?php
-session_start();
-
-function connectDb($dbname='', $login = 'root', $password = '', $port='3306', $host = 'localhost') {
-    try {
-        // Essaie de faire ce script...
-        $bdd = new PDO('mysql:host='.$host.';dbname='.$dbname.';charset=utf8;port='.$port, $login, $password);
-
-        return $bdd;
-    }
-    catch (Exception $e) {
-        // Sinon, capture l'erreur et affiche la
-        die('Erreur : ' . $e->getMessage());
-    }
-}
-
+require_once('helper.php');
 
 $bdd = connectDb('phpcourse', 'root', '', 3308);
-
 
 $response = $bdd->query('
         SELECT * 
